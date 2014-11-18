0815ad3469c7aed75f0e4b7590c54931fa269864
Brett Bumeter <brettbum@users.noreply.github.com>
2014-11-17 21:49:49 -0500

    Create functions.php
    This section should replace the default Registered Widgets area that comes in Parallax Pro.  The end result along with other file addendums will add 5 more parallax sections to the theme.

---
+++ functions.php
@@ -0,0 +1,109 @@
+//* Register widget areas
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-1',
+
+	'name'        => __( 'Home Section 1', 'parallax' ),
+
+	'description' => __( 'This is the home section 1 section.', 'parallax' ),
+
+) );
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-2',
+
+	'name'        => __( 'Home Section 2', 'parallax' ),
+
+	'description' => __( 'This is the home section 2 section.', 'parallax' ),
+
+) );
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-3',
+
+	'name'        => __( 'Home Section 3', 'parallax' ),
+
+	'description' => __( 'This is the home section 3 section.', 'parallax' ),
+
+) );
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-4',
+
+	'name'        => __( 'Home Section 4', 'parallax' ),
+
+	'description' => __( 'This is the home section 4 section.', 'parallax' ),
+
+) );
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-5',
+
+	'name'        => __( 'Home Section 5', 'parallax' ),
+
+	'description' => __( 'This is the home section 5 section.', 'parallax' ),
+
+) );
+
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-6',
+
+	'name'        => __( 'Home Section 6', 'parallax' ),
+
+	'description' => __( 'This is the home section 6 section.', 'parallax' ),
+
+) );
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-7',
+
+	'name'        => __( 'Home Section 7', 'parallax' ),
+
+	'description' => __( 'This is the home section 7 section.', 'parallax' ),
+
+) );
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-8',
+
+	'name'        => __( 'Home Section 8', 'parallax' ),
+
+	'description' => __( 'This is the home section 8 section.', 'parallax' ),
+
+) );
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-9',
+
+	'name'        => __( 'Home Section 9', 'parallax' ),
+
+	'description' => __( 'This is the home section 9 section.', 'parallax' ),
+
+) );
+genesis_register_sidebar( array(
+
+	'id'          => 'home-section-10',
+
+	'name'        => __( 'Home Section 10', 'parallax' ),
+
+	'description' => __( 'This is the home section 10 section.', 'parallax' ),
+
+) );
+
+
+
+genesis_register_sidebar( array(
+
+	'id'          => 'after-entry',
+
+	'name'        => __( 'After Entry', 'parallax' ),
+
+	'description' => __( 'This is the after entry widget area.', 'parallax' ),
+
+) );
